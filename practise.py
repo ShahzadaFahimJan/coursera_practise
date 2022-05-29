c57f9590c128a1aaa22b2cba878ef23528d35fc1
ShahzadaFahimJan <104683140+ShahzadaFahimJan@users.noreply.github.com>
2022-05-29 17:00:33 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -1,3 +1,15 @@
+#my notebook im writing in github because my  ibm account is not working
+#Shahzada Fahim Jan
+#100 % Human
+#im much interested in data science because i love data science and to gaing insights from data science 
+
 def gauss(n):
     return (n*(n+1))/2
 gauss(100)==sum(range(101))
+# output will be True
+.one 
+.two
+.three
+.one
+.two
+.three
